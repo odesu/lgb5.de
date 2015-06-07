a436d770283456a15de76319c191e60b9f48c1c6
Odesu <thimo.hoerster@odesu.de>
2015-06-07 15:06:49 +0200

    setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,20 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :rvm_type, :system
-set :rvm_ruby_version, '2.1.6'
-# set :rvm_custom_path, '/usr/local/rvm'
-
-#set :bundle_cmd, '/usr/local/rvm/gems/ruby-2.1.6/bin/bundle'
-#set :bundle_flags, ''
-
 set :application, 'dev.lgb5.de'
 set :deploy_to, '/home/www/applications/dev.lgb5.de'
 set :keep_releases, 5
 set :format, :pretty
 set :log_level, :debug
 set :repo_url, 'https://github.com/odesu/lgb5.de.git'
+set :rvm_type, :system
+set :rvm_ruby_version, '2.1.6'
 
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 namespace :deploy do
 
